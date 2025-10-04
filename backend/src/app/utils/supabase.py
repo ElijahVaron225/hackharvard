2161b88f8e747a6bae192b9cb0ba7f5119d6ad50
Ashwin Balaraman <abalaraman1@uchicago.edu>
2025-10-04 01:19:35 -0400

    supabase

---
+++ backend/src/app/utils/supabase.py
@@ -0,0 +1,8 @@
+# Import the supabse creds from the config
+from app.core.config import settings
+from supabase import create_client, Client
+
+# Create a function to return supabase client
+def get_client() -> Client:
+    supabase: Client = create_client(settings.SUPABASE_URL, settings.SUPABASE_KEY)
+    return supabase
